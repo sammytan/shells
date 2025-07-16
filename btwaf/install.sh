4267b1d11884d5ad9adf7bb16ca6fd13bcab61ab
Ken <Ken@bf11.cc>
2025-07-16 16:33:02 +0700

    update

--- btwaf/install.sh
+++ btwaf/install.sh
@@ -1,8 +1,16 @@
 #!/bin/env bash
 tar -xvf btwaf.tar.gz --overwrite -C /www/server/  && cp -r btwaf.conf /www/server/panel/vhost/nginx/
 
-
-
 ! test -f /www/server/whitelist.txt && touch  /www/server/whitelist.txt
+
+cp -rp ./owasp-modsec-crs  /www/server/nginx/conf/
+cp unicode.mapping /www/server/nginx/conf/
+cp modsecurity.conf  /www/server/nginx/conf/
+cp modsec_includes.conf  /www/server/nginx/conf/
+cp custom_modsec_rules.conf /www/server/nginx/conf/
+
+
+/bin/cp -r nginx.conf /www/server/nginx/conf/
+
 
 
